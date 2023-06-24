4dab352edd598ac2c447188851c80f1d7369d45a
joetid09 <joseph.tidwell09@gmail.com>
2023-06-24 14:55:06 -0500

    Wired up the chatgpt call and response to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ai-recipe-maker';
-  performSearch(searchTerm: string): void {
-    // Perform the search operation using the provided search term
-    console.log('Performing search with term:', searchTerm);
-    // You can update component state, call an API, or perform any other search-related actions
+  searchResult: string = '';
+
+  async performSearch(searchTerm: string): Promise<void> {
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions',
+      {
+        messages: [{ "role": "user", "content": "Provide me a recipe that uses " + searchTerm + "." }],
+        "model": "gpt-3.5-turbo"
+      },
+      {
+        headers: {
+          'Authorization': 'Bearer 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',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      this.searchResult = response.data.choices[0].message.content; // Extract the desired information from the response
+    } catch (error) {
+      console.error('Error performing search:', error);
+    }
   }
 }
